1f1b12ee550a68abe7f3939699e62e857cdaee87
jmarcoscosta <jmcosta944@gmail.com>
2017-08-11 16:24:42 -0300

    Formatação parcial H02 Python

--- CODES/HD_02_PYTHON/handson2_1.py
+++ CODES/HD_02_PYTHON/handson2_1.py
@@ -4,10 +4,9 @@ import matplotlib.pyplot as plt
 import scipy.io.wavfile as wv 
 import os
 
-# Parâmetros da onda
-tf = 1                   # Tempo de duração da nota
-fc = 512                 # Frequência da nota Dó
-fs = 100*fc              # Frequencia de amostragem da nota. 
+tf = 1                                  # Tempo de duração da nota
+fc = 512                                # Frequência da nota Dó
+fs = 100*fc                  # Frequencia de amostragem da nota. 
 t =np.arange(0,tf+1/fs,1/fs) # Vetor tempo. Para cada elemento do vetor t, haverá um elemento em y correspondente.
 A = 1                    # Amplitude do sinal
 y=A*np.cos(2*np.pi*fc*t)       # Sinal senoidal
@@ -19,12 +18,13 @@ plt.show()
 #DESCOMENTE AS LINHAS DE COMANDO ABAIXO APÓS AJUSTAR O ENDEREÇO, COMO SUGERIDO
 
 #importando e armazenando o arquivo de áudio numa variável
-#som = wv.read('./MATERIAL/HD_02_MATLAB/sound_01.wav')
+fs_som, som = wv.read('../../MATERIAL/HD_02_MATLAB/sound_01.wav')
 
 #salvando o tom gerado em um arquivo de extensão .wav :
-#wv.write('./MATERIAL/HD_02_PYTHON/tom_gerado.wav',fs,y)
+wv.write('../../MATERIAL/HD_02_PYTHON/tom_gerado.wav',fs,y)
 #reproduzindo o arquivo
 
-#os.system('cvlc ./MATERIAL/HD_02_PYTHON/tom_gerado.wav') 
+os.system('cvlc ../../MATERIAL/HD_02_PYTHON/tom_gerado.wav') 
 #vlc chama o programa VLC Audio Player
 #'c' serve para que nenhuma interface seja aberta
+
